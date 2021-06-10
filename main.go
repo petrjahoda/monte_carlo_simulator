dd1c94cc9da15713539bf7f0262133b7212db240
petrjahoda <petrjahoda@protonmail.com>
2021-06-10 09:54:06 +0200

    updated
    Took 1 hour 48 minutes

--- main.go
+++ main.go
@@ -2,55 +2,99 @@ package main
 
 import (
 	"fmt"
-	"github.com/leekchan/accounting"
-	"math"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	ac := accounting.NewAccounting("CZK", 2, " ", ".", "%v %s", "%s (%v)", "%s --")
-	initialValue := 14300.0
-	daysOfCalculation := 21
-	losses := 5
-	profits := 57
-	breakevens := 10
-	profitPercentage := profits * 100 / (profits + losses + breakevens)
-	averageLossInPips := -12.0
-	for i := 0; i < 10; i++ {
-		averageWinInPips := 18.0
-		var averages []float64
-		for i := 0; i < daysOfCalculation; i++ {
-			var numbers []float64
-			numbers = append(numbers, 0.0)
-			latestNumber := 0.0
+	rand.Seed(time.Now().UnixNano())
+	numberOfTrades := 100
+	spreadWithCommission := 1.1
+	totalSpread := float64(numberOfTrades) * spreadWithCommission
+	losses := 40
+	wins := 60
+	profitPercentage := wins * 100 / (wins + losses)
+	averageLossInPips := -26.5
+	averageWinInPips := 28.3
+	numberOfSimulations := 1000
+	randomDrawDown := rand.Intn(numberOfTrades-1) + 1
+	randomDrawDownInPips := -averageWinInPips * 2
+	randomSlippage := rand.Intn(numberOfTrades-1) + 1
+	randomSlippageInPips := -averageWinInPips / 2
+	randomBadDay := rand.Intn(numberOfTrades-1) + 1
 
-			for i := 0; i < daysOfCalculation; i++ {
-				randomProduction := rand.Intn(100-0) + 0
-				if randomProduction < profitPercentage {
-					latestNumber += averageWinInPips
-				} else {
-					latestNumber += averageLossInPips
-				}
-				numbers = append(numbers, latestNumber)
-			}
-			//fmt.Println(numbers[daysOfCalculation])
-			averages = append(averages, numbers[daysOfCalculation])
+	var listOfResults []float64
+
+	for i := 0; i < numberOfSimulations; i++ {
+		fmt.Print("Monte Carlo Simulation No.      ")
+		fmt.Printf("%d\n", i+1)
+		if randomBadDay == rand.Intn(numberOfTrades-1)+1 {
+			profitPercentage = 100 - profitPercentage
+			fmt.Print("B")
 		}
 		sum := 0.0
-		for _, average := range averages {
-			sum += average
+		profitsCount := 0
+		lossesCount := 0
+		for i := 0; i < numberOfTrades; i++ {
+			randomProduction := rand.Intn(numberOfTrades-1) + 1
+			if randomProduction < profitPercentage {
+				fmt.Print("+")
+				profitsCount++
+				sum += averageWinInPips
+				if randomProduction == randomSlippage {
+					sum += randomSlippageInPips
+					fmt.Print("S")
+				}
+			} else {
+				fmt.Print("-")
+				lossesCount++
+				sum += averageLossInPips
+				if randomProduction == randomSlippage {
+					sum += randomSlippageInPips
+					fmt.Print("S")
+				}
+			}
+		}
+		if randomDrawDown == rand.Intn(numberOfTrades-1)+1 {
+			sum += randomDrawDownInPips
+			fmt.Print("+!!")
 		}
 		fmt.Println("")
+		sum -= totalSpread
+		fmt.Print("Number of profits               ")
+		fmt.Printf("%d\n", profitsCount)
+		fmt.Print("Number of losses                ")
+		fmt.Printf("%d\n", lossesCount)
+		fmt.Print("Total sum of pips               ")
+		fmt.Printf("%.1f pips\n", sum)
+		fmt.Print("Average trade will give you     ")
+		fmt.Printf("%.1f pips\n", sum/float64(numberOfTrades))
 		fmt.Println("")
-		fmt.Print("Total average sum of pips after set days of calculation:  ")
-		fmt.Println(int(sum / float64(daysOfCalculation)))
-		fmt.Print("Average daily sum of pips after set days of calculation:  ")
-		fmt.Printf("%.2f pips\n", sum/float64(daysOfCalculation)/float64(daysOfCalculation))
-		fmt.Print("Daily percentage estimate after set days of calculation:  ")
-		fmt.Printf("%.2f %%\n", 3.79/18.0*(sum/float64(daysOfCalculation)/float64(daysOfCalculation)-1))
-		fmt.Print("Total value after set days:                               ")
-		fmt.Printf(ac.FormatMoney(initialValue * math.Pow(1+(3.79/18.0*(sum/float64(daysOfCalculation)/float64(daysOfCalculation)-1))/100, float64(daysOfCalculation))))
-
+		listOfResults = append(listOfResults, sum)
 	}
-
+	totalSum := 0.0
+	min := 0.0
+	max := 0.0
+	for i, result := range listOfResults {
+		if i == 0 {
+			min = result
+			max = result
+		}
+		totalSum += result
+		if result < min {
+			min = result
+		}
+		if result > max {
+			max = result
+		}
+	}
+	average := totalSum / float64(len(listOfResults))
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Print("Average sum                         ")
+	fmt.Printf("%.1f pips\n", average)
+	fmt.Print("Min sum                             ")
+	fmt.Printf("%.1f pips\n", min)
+	fmt.Print("Max sum                             ")
+	fmt.Printf("%.1f pips\n", max)
 }
